12355bfe2ffcd2679ed8af4d262cda3febf4e9ce
llama245 <lorenbap77@outlook.com>
2023-02-15 18:36:44 -0500

    repaired playRound function, fully functional now

--- script.js
+++ script.js
@@ -26,83 +26,74 @@ function playRound (computerSelection,playerSelection) {
     
 
     if (var1 == var2) {
-        let tie = "It's a tie!"
-        return tie 
+        return "It's a tie!"
     }
 
-    else if (var1 === "scissors" && var2 == "paper") {
-        let scissors_paper_winner = "You Win! Scissors beats Paper"
+    else if (var1 === "scissors" && var2 === "paper") {
+        return "You Win! Scissors beats Paper"
         
-        return scissors_paper_winner
     } 
 
-    else if (var1 === "scissors" && var2 == "rock") {
-        let scissors_rock_winner = "You Lose! Rock beats Scissors"
-        
-        return scissors_rock_winner
+    else if (var1 === "scissors" && var2 === "rock") {
+        return "You Lose! Rock beats Scissors" 
     } 
     
-    else if (var1 === "paper" && var2 == "rock") {
-        paper_rock_winner = "You win! Paper beats Rock"
-       
-        return paper_rock_winner
+    else if (var1 === "paper" && var2 === "rock") {
+        return "You win! Paper beats Rock"  
     } 
     
-    else if (var1 == "paper" && var2 == "scissors") {
-        paper_scissors_loser = "You Lose! Scissors beats Paper"
-      
-        return paper_scissors_loser
+    else if (var1 === "paper" && var2 === "scissors") {
+        return "You Lose! Scissors beats Paper"
     }
 
-    else if (var1 == "rock" && var2 == "paper") {
-        let rock_paper_loser = "You Lose! Paper beats Rock"
-        
-        return rock_paper_loser
+    else if (var1 === "rock" && var2 === "paper") {
+        return "You Lose! Paper beats Rock"
+
     }
 
-    else if (var1 == "rock" && var2 === "scissors") {
-        let rock_scissors_loser = "You Win! Rock beats Scissors"
-        
-        return rock_scissors_loser
+    else if (var1 === "rock" && var2 === "scissors") { 
+        return "You Win! Rock beats Scissors"
     } 
 
 }
 
 function game() {
+    let player_score = 0 
+    let computer_score = 0 
 
+    let sub_1 = "Win"
+    let sub_2 = "Lose"
+    let sub_3 = "tie"
+    
     for (let i = 0; i < 5; i++) {
 
-        if (proof_player = true ) {
-            player += 1
-        }
-
-        else if (proof_computer = true) {
-            computer += 1
-        }
+        const result = playRound(getComputerChoice(),getHumanChoice())
+        let proof = result 
         
-        else if (proof_tie = true) {
-            player += 1 
-            computer += 1
+        if (proof.includes(sub_1) == true) {
+        player_score += 1
         }
-
+        else if (proof.includes(sub_2) == true) {
+        computer_score += 1
+        }
+        else if (proof.includes(sub_3) == true) {
+        player_score += 1
+        computer_score += 1
+        }
     }
-
-    if (player > computer) {
+    if (player_score > computer_score) {
         console.log("Player Wins!")
-    }
-
-    else if (computer > player) {
-        
+    } 
+    else if (computer_score > player_score) {
         console.log("Computer Wins!")
     }
 } 
 
 
     
-const computerSelection = getComputerChoice()
-const playerSelection = getHumanChoice()
+
+game()
 
 
 
-console.log(result)
-game(playRound(computerSelection,playerSelection)) 
+
